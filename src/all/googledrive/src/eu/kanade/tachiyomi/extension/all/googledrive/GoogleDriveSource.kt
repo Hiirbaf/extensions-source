6a5c9275a85375f12b42e63009e305ffd981bf2b
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-01 20:04:44 -0300

    Update GoogleDriveSource.kt

--- src/all/googledrive/src/eu/kanade/tachiyomi/extension/all/googledrive/GoogleDriveSource.kt
+++ src/all/googledrive/src/eu/kanade/tachiyomi/extension/all/googledrive/GoogleDriveSource.kt
@@ -1,15 +1,15 @@
 package eu.kanade.tachiyomi.extension.all.googledrive
 
+import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
-import eu.kanade.tachiyomi.network.GET
-import eu.kanade.tachiyomi.source.model.FilterList
+import okhttp3.Request
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import okhttp3.Request
 
 class GoogleDriveSource : ParsedHttpSource() {
 
