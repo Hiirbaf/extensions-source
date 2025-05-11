4f3f7b21fb151432a7d1372c4e404135a80d560c
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-10 21:50:17 -0300

    Update Marmota.kt

--- src/es/marmota/src/eu/kanade/tachiyomi/extension/es/marmota/Marmota.kt
+++ src/es/marmota/src/eu/kanade/tachiyomi/extension/es/marmota/Marmota.kt
@@ -1,6 +1,8 @@
 package eu.kanade.tachiyomi.extension.es.marmota
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.source.model.SManga
+import org.jsoup.nodes.Document
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -10,7 +12,23 @@ class Marmota : Madara(
     "es",
     dateFormat = SimpleDateFormat("d 'de' MMM 'de' yyyy", Locale("es")),
 ) {
-    override val mangaSubString: String = "Comic"
+    override val mangaSubString: String = "comic"
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Never
+
+    override fun mangaDetailsParse(document: Document): SManga {
+        val manga = super.mangaDetailsParse(document)
+
+        val genres = manga.genre.orEmpty()
+            .split(", ")
+            .map { it.trim() }
+            .toMutableList()
+
+        if (genres.none { it.contains("comic", ignoreCase = true) }) {
+            genres.add("Comic")
+        }
+
+        manga.genre = genres.joinToString(", ")
+        return manga
+    }
 }
