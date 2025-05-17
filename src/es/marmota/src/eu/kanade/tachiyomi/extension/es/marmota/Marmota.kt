090101fdaf0defc1a38bb5d6d752a7a4e3bd98ed
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-05-17 15:51:26 -0300

    Update Marmota.kt

--- src/es/marmota/src/eu/kanade/tachiyomi/extension/es/marmota/Marmota.kt
+++ src/es/marmota/src/eu/kanade/tachiyomi/extension/es/marmota/Marmota.kt
@@ -13,4 +13,22 @@ class Marmota : Madara(
     override val mangaSubString: String = "comic"
     override val useNewChapterEndpoint = true
     override val useLoadMoreRequest = LoadMoreStrategy.Never
+
+    override fun mangaDetailsParse(document: Document): SManga {
+    val manga = super.mangaDetailsParse(document)
+
+    val altNameElement = document.selectFirst(altNameSelector)
+    val altNameText = altNameElement?.ownText()
+
+    if (!altNameText.isNullOrBlank() && altNameText.notUpdating()) {
+        val formattedAltName = "<b>$altName $altNameText</b>"
+
+        manga.description = when {
+            manga.description.isNullOrBlank() -> formattedAltName
+            else -> "${manga.description}\n\n$formattedAltName"
+        }
+    }
+
+    return manga
+    }
 }
