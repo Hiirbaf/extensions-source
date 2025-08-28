8dc3cf5a9c2655495a96a802595a1b2246772d81
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-28 19:06:33 -0300

    Update NOVA.kt

--- src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
+++ src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
@@ -133,7 +133,8 @@ class NOVA : ParsedHttpSource() {
             // Quitar <p> que solo contienen una imagen
             contentElement.select("p").forEach { p ->
                 if (p.childrenSize() == 1 && p.child(0).tagName() == "img" && p.ownText().isBlank()) {
-                    p.unwrap()
+                    val img = p.child(0) // el <img>
+                    p.replaceWith(img)   // reemplazar el <p> por el <img>
                 }
             }
             // Quitar párrafos vacíos o con solo &nbsp;
