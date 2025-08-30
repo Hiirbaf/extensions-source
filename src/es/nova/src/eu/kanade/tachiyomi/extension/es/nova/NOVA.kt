b67ea5b08afd4f83aff187d3aee365a65ed54d9f
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-30 17:41:56 -0300

    Container

--- src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
+++ src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
@@ -71,8 +71,9 @@ class NOVA : ParsedHttpSource() {
         author = document.detail(".woocommerce-product-attributes-item--attribute_pa_escritor td")
         artist = document.detail(".woocommerce-product-attributes-item--attribute_pa_ilustrador td")
         description = document.select(".woocommerce-product-details__short-description").text()
-        val genres = document.select(".product_meta .posted_in a").map { it.text() }
-        val labels = document.select(".berocket_better_labels b").map { it.text() }
+        val container = document.selectFirst("div.product.type-product") ?: document.body()
+        val genres = container.select(".product_meta .posted_in a").map { it.text().trim() }
+        val labels = container.select(".berocket_better_labels b").map { it.text().trim() }
         genre = (labels + genres).joinToString(", ")
         status = when (document.detail(".woocommerce-product-attributes-item--attribute_pa_estado td")?.lowercase()) {
             "en curso", "ongoing" -> SManga.ONGOING
