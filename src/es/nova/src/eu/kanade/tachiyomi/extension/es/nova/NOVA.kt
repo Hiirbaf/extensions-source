8f283610b74f2a541b394b94fb00b335184460ea
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-08-31 03:38:27 -0300

    Update NOVA.kt

--- src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
+++ src/es/nova/src/eu/kanade/tachiyomi/extension/es/nova/NOVA.kt
@@ -6,6 +6,8 @@ import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.Request
+import okhttp3.Response
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 import java.net.URLEncoder
