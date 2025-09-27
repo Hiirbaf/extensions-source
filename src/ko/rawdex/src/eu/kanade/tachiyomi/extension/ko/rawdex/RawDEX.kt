e9b191262a576cf11bfd7db7154f6c27ba63c99d
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-09-26 21:50:33 -0300

    Update RawDEX.kt

--- src/ko/rawdex/src/eu/kanade/tachiyomi/extension/ko/rawdex/RawDEX.kt
+++ src/ko/rawdex/src/eu/kanade/tachiyomi/extension/ko/rawdex/RawDEX.kt
@@ -1,5 +1,16 @@
 package eu.kanade.tachiyomi.extension.ko.rawdex
 
 import eu.kanade.tachiyomi.multisrc.madara.Madara
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class RawDEX : Madara("RawDEX", "https://rawdex.net", "ko")
+class RawDEX : Madara(
+    "RawDEX",
+    "https://rawdex.net",
+    "ko",
+    dateFormat = SimpleDateFormat("d 'de' MMM 'de' yyyy", Locale("ko")),
+) {
+    override val mangaSubString: String = "manhwa"
+    override val useNewChapterEndpoint = true
+    override val useLoadMoreRequest = LoadMoreStrategy.Never
+}
