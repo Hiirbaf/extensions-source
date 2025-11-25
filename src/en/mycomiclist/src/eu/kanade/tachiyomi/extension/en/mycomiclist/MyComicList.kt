21b61bcb98c31723cf4d71e77dc463413d68b9e9
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-11-25 19:58:58 -0300

    Clean up MyComicList.kt by removing imports
    Removed unused imports from MyComicList.kt

--- src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
+++ src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
@@ -1,18 +1,15 @@
 package eu.kanade.tachiyomi.extension.en.mycomiclist
 
-import eu.kanade.tachiyomi.network.asObservableSuccess
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.ConfigurableSource
 import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
-import eu.kanade.tachiyomi.source.model.MangasPage
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
 import okhttp3.Request
 import okhttp3.Response
-import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
 class MyComicList : ParsedHttpSource(), ConfigurableSource {
