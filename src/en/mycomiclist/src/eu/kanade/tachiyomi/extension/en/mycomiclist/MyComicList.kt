3833b7f4c1526e280b25538ae3914353dc80e299
Hiirbaf <42479509+Hiirbaf@users.noreply.github.com>
2025-11-25 20:03:23 -0300

    Fix import order in MyComicList.kt

--- src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
+++ src/en/mycomiclist/src/eu/kanade/tachiyomi/extension/en/mycomiclist/MyComicList.kt
@@ -2,12 +2,12 @@ package eu.kanade.tachiyomi.extension.en.mycomiclist
 
 import eu.kanade.tachiyomi.network.GET
 import eu.kanade.tachiyomi.source.ConfigurableSource
-import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import eu.kanade.tachiyomi.source.model.Filter
 import eu.kanade.tachiyomi.source.model.FilterList
 import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.source.model.SChapter
 import eu.kanade.tachiyomi.source.model.SManga
+import eu.kanade.tachiyomi.source.online.ParsedHttpSource
 import okhttp3.Request
 import okhttp3.Response
 import org.jsoup.nodes.Element
